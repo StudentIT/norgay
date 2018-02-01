652ef51c875fcfec704f83637ad9a12fccca3eab
Pieter Maene <pieter@maene.eu>
2018-02-01 18:56:06 +0100

    Localization

--- main.go
+++ main.go
@@ -1,117 +1,130 @@
 package main
 
 import (
-    "encoding/json"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
-    "path/filepath"
-    "time"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
-    "github.com/gin-gonic/gin"
-    "github.com/spf13/viper"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 const (
-    UpdatePeriod = time.Minute
+	UpdatePeriod = time.Minute
 )
 
 type CurrentSession struct {
-    Status string
+	Status string
 }
 
 type Store struct {
-    Name   string
-    Host   string
-    Status string
+	Name   map[string]string
+	Host   string
+	Status string
 }
+
 func (s *Store) GetStatus(key string) error {
-    req, err := http.NewRequest("GET", "https://" + s.Host + "/api/cudi/currentSession", nil)
-    if err != nil {
-        return err
-    }
+	req, err := http.NewRequest("GET", "https://"+s.Host+"/api/cudi/currentSession", nil)
+	if err != nil {
+		return err
+	}
 
-    q := req.URL.Query()
-    q.Add("key", key)
-    req.URL.RawQuery = q.Encode()
+	q := req.URL.Query()
+	q.Add("key", key)
+	req.URL.RawQuery = q.Encode()
 
-    c := http.Client{}
+	c := http.Client{}
 
-    res, err := c.Do(req)
-    if err != nil {
-        return err
-    }
-    defer res.Body.Close()
+	res, err := c.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
 
-    b, err := ioutil.ReadAll(res.Body)
-    if err != nil {
-        return err
-    }
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
 
-    var cs CurrentSession
-    if err := json.Unmarshal(b, &cs); err != nil {
-        return err
-    }
-    s.Status = cs.Status
+	var cs CurrentSession
+	if err := json.Unmarshal(b, &cs); err != nil {
+		return err
+	}
+	s.Status = cs.Status
 
-    return nil
+	return nil
 }
 
 func UpdateStores() ([]Store, error) {
-    var stores []Store
-    for _, store := range viper.Get("stores").([]interface{}) {
-        m := store.(map[string]interface{})
+	var stores []Store
+	for _, store := range viper.Get("stores").([]interface{}) {
+		s := store.(map[string]interface{})
 
-        s := Store{m["name"].(string), m["host"].(string), ""}
-        if err := s.GetStatus(m["key"].(string)); err != nil {
-            return nil, err
-        }
+		l := make(map[string]string, len(s["name"].(map[string]interface{})))
+		for k, v := range s["name"].(map[string]interface{}) {
+			l[k] = v.(string)
+		}
 
-        stores = append(stores, s)
-    }
+		store := Store{l, s["host"].(string), ""}
+		if err := store.GetStatus(s["key"].(string)); err != nil {
+			return nil, err
+		}
 
-    return stores, nil
+		stores = append(stores, store)
+	}
+
+	return stores, nil
 }
 
 func main() {
-    viper.SetConfigName("config")
-    viper.AddConfigPath("$HOME/.config/norgay")
-    viper.AddConfigPath(".")
+	viper.SetConfigName("config")
+	viper.AddConfigPath("$HOME/.config/norgay")
+	viper.AddConfigPath(".")
 
-    if err := viper.ReadInConfig(); err != nil {
-        panic(err)
-    }
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
 
-    e, err := os.Executable()
-    if err != nil {
-        panic(err)
-    }
-    d := filepath.Dir(e)
+	e, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	d := filepath.Dir(e)
 
-    var s []Store
-    go func() {
-        for {
-            us, err := UpdateStores()
-            if err != nil {
-                log.Printf("[norgay] %s\n", err)
-            }
+	var s []Store
+	go func() {
+		for {
+			us, err := UpdateStores()
+			if err != nil {
+				log.Printf("[norgay] %s\n", err)
+			}
 
-            s = us
-            log.Println("[norgay] stores updated")
+			s = us
+			log.Println("[norgay] updated stores")
 
-            time.Sleep(UpdatePeriod)
-        }
-    }()
+			time.Sleep(UpdatePeriod)
+		}
+	}()
 
-    r := gin.Default()
-    r.LoadHTMLFiles(d + "/templates/index.tmpl")
+	r := gin.Default()
+	r.LoadHTMLFiles(d + "/templates/index.tmpl")
 
 	r.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"Stores": s,
+		l := c.GetHeader("Accept-Language")
+		if _, ok := viper.GetStringMapString("title")[l]; !ok {
+			l = "en-us"
+		}
+
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"Language": l,
+			"Title":    viper.GetStringMapString("title"),
+			"Stores":   s,
 		})
 	})
 
-    r.Run()
+	r.Run()
 }
